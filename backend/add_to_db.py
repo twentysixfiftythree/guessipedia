1e31d3a7747bea3741f61f8ca87e5687ddc1c511
twentysixfiftythree <20ipw@queensu.ca>
2024-02-29 15:46:13 -0500

    cleaning up old stuff to make room for front end

--- backend/add_to_db.py
+++ backend/add_to_db.py
@@ -1,19 +1,17 @@
 from data_fetch import fetch_wikipedia_page_details
 from database_interaction import add_wikipedia_page
 
-
 def main():
     urls = [
-        "https://en.wikipedia.org/wiki/Python_(programming_language)",
-        "https://en.wikipedia.org/wiki/Flask_(web_framework)",
-        "https://en.wikipedia.org/wiki/Canada",
+        'https://en.wikipedia.org/wiki/Python_(programming_language)',
+        'https://en.wikipedia.org/wiki/Flask_(web_framework)',
+        'https://en.wikipedia.org/wiki/Canada'
     ]
-
+    
     # Example URLs
     for url in urls:
         page_data = fetch_wikipedia_page_details(url)
         add_wikipedia_page(page_data)
-
 
 if __name__ == "__main__":
     main()
